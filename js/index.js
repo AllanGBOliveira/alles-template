a3400cc95e02c1acfcf2615218e7a278a71b8a72
Your Name <you@example.com>
2021-02-26 14:35:48 -0300

    first-filtes

--- js/index.js
+++ js/index.js
@@ -15,3 +15,12 @@ window.onclick = function(event) {
         }
     }
 }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
